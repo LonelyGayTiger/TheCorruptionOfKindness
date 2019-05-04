0a758975735c1ec1d9c4af3fe30d93855d85592b
LonelyGayTiger <lonelygaytiger@outlook.com>
2019-05-04 02:41:51 -0700

    commit for merge

--- src/com/CorruptionOfKindness/Creatures/Bat.java
+++ src/com/CorruptionOfKindness/Creatures/Bat.java
@@ -4,6 +4,8 @@ import com.CorruptionOfKindness.Core.Entity;
 import com.CorruptionOfKindness.CoreInterfaces.AfterInit;
 import com.CorruptionOfKindness.CoreInterfaces.Drop;
 
+//am totally using this just for a commit so I can merge
+
 public class Bat extends Entity implements AfterInit, Drop {
 
 	public Bat(com.CorruptionOfKindness.Core.GameState GameState, boolean player) {
